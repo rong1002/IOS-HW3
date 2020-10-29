dcbc24e8b098442b38226d5a6c80a2309a5d49df
rong1002 <borong1002@gmail.com>
2020-10-29 15:42:40 +0800

    Initial Commit

---
+++ IOS-HW3/IOS_HW3App.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_HW3App.swift
+//  IOS-HW3
+//
+//  Created by Lin Bo Rong on 2020/10/29.
+//
+
+import SwiftUI
+
+@main
+struct IOS_HW3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
